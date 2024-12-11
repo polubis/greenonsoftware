135f5dd95524f210f80caa57c362a7ada40246c5
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-12-11 11:43:19 +0100

    chore(app): configure make-me-senior app

---
+++ greenonsoftware/apps/make-me-senior/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'make-me-senior',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/make-me-senior',
+};
