d850f984778a9f75540a4ad4d01bf894fd28c4da
Adrian Połubiński <adrian.polubinski@billennium.com>
2024-12-11 15:24:41 +0100

    feat(app): add basic home page for todo app

---
+++ documentation/5/creating-local-libraries.md
@@ -0,0 +1,4 @@
+1. To generate a library to be used internally in the monorepo, type `nx g @nx/js:lib libs/name-of-lib`.
+2. Here is what I've picked up in CLI: 
+
+![alt text](image.png)
--- greenonsoftware/apps/todo/tailwind.config.js
+++ greenonsoftware/apps/todo/tailwind.config.js
@@ -1,5 +1,6 @@
 const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
 const { join } = require('path');
+const plugin = require('tailwindcss/plugin');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -13,5 +14,23 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addComponents }) => {
+      addComponents({
+        '.xy-divide': {
+          position: 'relative',
+          '&::before, &::after': {
+            content: "''",
+            '@apply absolute bg-gray-300': {},
+          },
+          '&::before': {
+            '@apply w-0.5 top-0 bottom-0 -left-[1px] right-0 mx-auto': {},
+          },
+          '&::after': {
+            '@apply h-0.5 -top-[1px] bottom-0 left-0 right-0 my-auto': {},
+          },
+        },
+      });
+    }),
+  ],
 };
