947e94068e8650201b1aab7dd05233e5fd3d2767
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-01-27 11:02:19 +0100

    wip

--- apps/diet-app/src/server/auth.ts
+++ apps/diet-app/src/server/auth.ts
@@ -1,25 +1,19 @@
-import { NextResponse } from 'next/server';
 import { getApp } from './app';
 
 function getToken(request: Request) {
-  const token = request.headers.get('Authorization')?.split('Bearer ')[1];
-  return token;
+  return request.headers.get('Authorization')?.split('Bearer ')[1];
 }
 
 export async function authenticate(request: Request) {
   const token = getToken(request);
 
   if (!token) {
-    return new NextResponse(
-      JSON.stringify({
-        message: 'Access blocked',
-        error: `Resource access blocked`,
-      }),
-      { status: 401, headers: { 'Content-Type': 'application/json' } }
-    );
+    throw Error(`Access denied`);
   }
 
-  const user = await getApp().auth().verifyIdToken(token);
-
-  console.log(user);
+  try {
+    return getApp().auth().verifyIdToken(token);
+  } catch {
+    throw Error(`Access denied`);
+  }
 }
