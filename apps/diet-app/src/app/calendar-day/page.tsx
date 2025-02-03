3c05c091d2df7ec76d68cdb8d23fa3240debe675
Adrian <klimasa98@gmail.com>
2025-02-03 09:10:50 +0100

    copy calendar component

---
+++ apps/diet-app/src/app/calendar-day/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Calendar } from '../../client/components/calendar'
+
+
+export default function CalendarDay() {
+  return (
+    <div>
+      <Calendar/>
+    </div>
+  )
+}
