e44cdcd1b51d0138689dad5861d193feaad64fb3
Adrian <klimasa98@gmail.com>
2025-02-05 14:29:43 +0100

    created footer component & add icons

--- apps/diet-app/src/app/calendar-day/page.tsx
+++ apps/diet-app/src/app/calendar-day/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Calendar } from '../../client/components/calendar/calendar';
+import Footer from '../../client/components/footer/footer';
 
 export default function CalendarDay() {
   return (
     <div>
       <Calendar />
+      <Footer />
     </div>
   );
 }
---
+++ apps/diet-app/src/client/components/footer/footer.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import React from 'react';
+import { AddBtn, BarChart, Book, Plan, ShoppingList } from './icons';
+
+const liStyle = 'flex flex-col items-center justify-center';
+const btnStyle = 'flex flex-col items-center justify-center focus:outline-none';
+
+export default function Footer() {
+  return (
+    <footer className="fixed bottom-0 w-screen bg-white">
+      <ul className="flex items-center justify-around p-4">
+        <li className={liStyle}>
+          <button
+            className={btnStyle}
+            onClick={() => console.log('Plan clicked')}
+          >
+            <Plan />
+            Plan
+          </button>
+        </li>
+        <li className={liStyle}>
+          <button
+            className={btnStyle}
+            onClick={() => console.log('Recipes clicked')}
+          >
+            <Book />
+            Recipes
+          </button>
+        </li>
+        <li className={liStyle}>
+          <button
+            className={btnStyle}
+            onClick={() => console.log('Add clicked')}
+          >
+            <AddBtn />
+          </button>
+        </li>
+        <li className={liStyle}>
+          <button
+            className={btnStyle}
+            onClick={() => console.log('Shopping clicked')}
+          >
+            <ShoppingList />
+            Shopping
+          </button>
+        </li>
+        <li className={liStyle}>
+          <button
+            className={btnStyle}
+            onClick={() => console.log('Progress clicked')}
+          >
+            <BarChart />
+            Progress
+          </button>
+        </li>
+      </ul>
+    </footer>
+  );
+}
