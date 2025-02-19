1ba39cd798f81148dfb54610ae108ed5ec009849
Adrian <klimasa98@gmail.com>
2025-02-19 06:24:27 +0100

    fix react hook form to show data after final submit, fix some styles, uninstall unused dependencies

--- apps/diet-app/src/client/components/diet-personalization-form/diet-personalization-fom.tsx
+++ apps/diet-app/src/client/components/diet-personalization-form/diet-personalization-fom.tsx
@@ -1,12 +1,16 @@
 'use client';
 
 import React, { useState } from 'react';
-import { useForm, Controller } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import Slider from '@mui/material/Slider';
 import HeaderWithBackArrow from '../header-with-back-arrow/header-with-back-arrow';
 
 export default function DietPersonalizationForm() {
-  const { control, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm({ mode: 'onChange' });
   const [step, setStep] = useState(1);
   const onSubmit = (data) => console.log(data);
 
@@ -25,119 +29,120 @@ export default function DietPersonalizationForm() {
             <label className="block mb-8 text-2xl font-semibold text-semibold">
               What's your goal?
             </label>
-            <Controller
-              name="goal"
-              control={control}
-              defaultValue=""
-              render={() => (
-                <div className="flex flex-col gap-2 text-left">
-                  <button className={buttonStyle}>
-                    <div className="flex items-center gap-4">
-                      <div className="w-4 h-4 border rounded-full"></div>
-                      <p className={pStyle}> Lose weight </p>
-                    </div>
-                    <p className={descriptionStyle}>
-                      Stay motivated with delicious, wholesome meals
-                    </p>
-                  </button>
-                  <button className={buttonStyle}>
-                    <div className="flex items-center gap-4">
-                      <div className="w-4 h-4 border rounded-full"></div>
-                      <p className={pStyle}> Maintain weight </p>
-                    </div>
-                    <p className={descriptionStyle}>
-                      Enjoy balanced meals that boost energy and support your
-                      health
-                    </p>
-                  </button>
-                  <button className={buttonStyle}>
-                    <div className="flex items-center gap-4">
-                      <div className="w-4 h-4 border rounded-full"></div>
-                      <p className={pStyle}> Gain muscle</p>
-                    </div>
-                    <p className={descriptionStyle}>
-                      Increase strength and enhance your physical performance
-                    </p>
-                  </button>
+            <div className="flex flex-col gap-2 text-left">
+              <label className={buttonStyle}>
+                <div className="flex items-center gap-4">
+                  <input
+                    type="radio"
+                    value="lose_weight"
+                    {...register('goal', { required: true })}
+                    className="w-4 h-4 border rounded-full"
+                  />
+                  <p className={pStyle}> Lose weight </p>
                 </div>
-              )}
-            />
+                <p className={descriptionStyle}>
+                  Stay motivated with delicious, wholesome meals
+                </p>
+              </label>
+              <label className={buttonStyle}>
+                <div className="flex items-center gap-4">
+                  <input
+                    type="radio"
+                    value="maintain_weight"
+                    {...register('goal', { required: true })}
+                    className="w-4 h-4 border rounded-full"
+                  />
+                  <p className={pStyle}> Maintain weight </p>
+                </div>
+                <p className={descriptionStyle}>
+                  Enjoy balanced meals that boost energy and support your health
+                </p>
+              </label>
+              <label className={buttonStyle}>
+                <div className="flex items-center gap-4">
+                  <input
+                    type="radio"
+                    value="gain_muscle"
+                    {...register('goal', { required: true })}
+                    className="w-4 h-4 border rounded-full"
+                  />
+                  <p className={pStyle}> Gain muscle</p>
+                </div>
+                <p className={descriptionStyle}>
+                  Increase strength and enhance your physical performance
+                </p>
+              </label>
+            </div>
           </div>
         );
       case 2:
         return (
           <div className="mb-4">
             <label className="block mb-8 text-2xl font-semibold text-semibold">
-             What's your biological sex?
+              What's your biological sex?
             </label>
-            <Controller
-              name="goal"
-              control={control}
-              defaultValue=""
-              render={() => (
-                <div className="flex flex-col gap-2 text-left">
-                  <button className={buttonStyle}>
-                    <div className="flex items-center gap-4">
-                      <div className="w-4 h-4 border rounded-full"></div>
-                      <p className={pStyle}> Male </p>
-                    </div>
-                 
-                  </button>
-                  <button  className={buttonStyle}>
-                    <div className="flex items-center gap-4">
-                      <div className="w-4 h-4 border rounded-full"></div>
-                      <p className={pStyle}> Female </p>
-                    </div>
-                 
-                  </button>
-              
+            <div className="flex flex-col gap-2 text-left">
+              <label className={buttonStyle}>
+                <div className="flex items-center gap-4">
+                  <input
+                    type="radio"
+                    value="male"
+                    {...register('sex', { required: true })}
+                    className="w-4 h-4 border rounded-full"
+                  />
+                  <p className={pStyle}> Male </p>
                 </div>
-              )}
-            />
+              </label>
+              <label className={buttonStyle}>
+                <div className="flex items-center gap-4">
+                  <input
+                    type="radio"
+                    value="female"
+                    {...register('sex', { required: true })}
+                    className="w-4 h-4 border rounded-full"
+                  />
+                  <p className={pStyle}> Female </p>
+                </div>
+              </label>
+            </div>
           </div>
         );
       case 3:
         return (
           <div className="mb-4">
-            <label className="block mb-2 text-sm font-bold text-gray-700">
+            <label className="block mb-8 text-2xl font-semibold text-semibold">
               Weight and Goal
             </label>
-            <Controller
-              name="weightGoal"
-              control={control}
-              defaultValue={50}
-              render={({ field }) => (
-                <Slider
-                  {...field}
-                  orientation="vertical"
-                  min={0}
-                  max={100}
-                  valueLabelDisplay="auto"
-                />
-              )}
-            />
+            <div className="flex flex-col items-center">
+              <Slider
+                {...register('weightGoal', { required: true })}
+                defaultValue={50}
+                orientation="horizontal"
+                min={0}
+                max={100}
+                valueLabelDisplay="auto"
+                className="w-full"
+              />
+            </div>
           </div>
         );
       case 4:
         return (
           <div className="mb-4">
-            <label className="block mb-2 text-sm font-bold text-gray-700">
+            <label className="block mb-8 text-2xl font-semibold text-semibold">
               Age
             </label>
-            <Controller
-              name="age"
-              control={control}
-              defaultValue={25}
-              render={({ field }) => (
-                <Slider
-                  {...field}
-                  orientation="vertical"
-                  min={0}
-                  max={100}
-                  valueLabelDisplay="auto"
-                />
-              )}
-            />
+            <div className="flex flex-col items-center">
+              <Slider
+                {...register('age', { required: true })}
+                defaultValue={25}
+                orientation="horizontal"
+                min={0}
+                max={100}
+                valueLabelDisplay="auto"
+                className="w-full"
+              />
+            </div>
           </div>
         );
       default:
@@ -155,10 +160,10 @@ export default function DietPersonalizationForm() {
         }}
         title="Personalize your diet"
       />
-      <div className="w-full h-2 my-4 rounded-md progress-bar bg-lime-100">
+      <div className="w-full h-1 my-4 rounded-lg progress-bar bg-lime-100">
         <div
           style={{ width: `${progress}%` }}
-          className="h-full rounded-md bg-lime-200"
+          className="h-full rounded-md bg-lime-300"
         ></div>
       </div>
       <form
@@ -172,13 +177,17 @@ export default function DietPersonalizationForm() {
           <button
             type="button"
             onClick={nextStep}
-            className="w-full px-4 py-4 font-bold text-white bg-black rounded-full"
+            className={`w-full px-4 py-4 font-bold text-white rounded-full ${
+              isValid ? 'bg-black' : 'bg-gray-400'
+            }`}
+            disabled={!isValid}
           >
             Next
           </button>
         )}
         {step === 4 && (
           <button
+            onClick={() => handleSubmit(onSubmit)()}
             type="submit"
             className="w-full px-4 py-4 font-bold text-white bg-green-500 rounded hover:bg-green-700"
           >
