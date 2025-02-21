643a5efa81640c74ae93eee9483661dcab781d55
Adrian Połubiński <adrian.polubinski@billennium.com>
2025-02-21 10:39:33 +0100

    wip

--- libs/react-kit/src/lib/use-simple-feature.ts
+++ libs/react-kit/src/lib/use-simple-feature.ts
@@ -2,21 +2,7 @@ import { useMemo, useState } from 'react';
 
 type Setter<TState> = TState | (() => TState);
 
-type SimpleFeatureActions = {
-  on(): void;
-  off(): void;
-  toggle(): void;
-  reset(): void;
-  set(setter: Setter<boolean>): void;
-};
-type SimpleFeature = SimpleFeatureActions & {
-  isOff: boolean;
-  isOn: boolean;
-};
-
-const useSimpleFeature = (
-  defaultState: Setter<boolean> = false
-): SimpleFeature => {
+const useSimpleFeature = (defaultState: Setter<boolean> = false) => {
   const [initState] = useState(defaultState);
   const [isOn, setIsOn] = useState(initState);
 
